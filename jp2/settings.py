38770195a324785a6b981e22bebbf139b50d0c4e
jirkha <jirkha13@seznam.cz>
2022-12-31 11:02:46 +0100

    heroku fix issues

--- jp2/settings.py
+++ jp2/settings.py
@@ -160,7 +160,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    #BASE_DIR / 'static',
+    BASE_DIR / 'static',
     BASE_DIR / 'jp2_react/build/static'
 ]
 
